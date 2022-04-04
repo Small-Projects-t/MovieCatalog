1044726f8ef4a9abb733ddf3a7571ee9fda4415b
Yoan <y.dimitrov@infoleven.com>
2022-04-04 12:18:04 +0300

    autocomplete input search fixed

--- src/components/AddEditMovie.tsx
+++ src/components/AddEditMovie.tsx
@@ -3,10 +3,10 @@ import InputLabel from '@mui/material/InputLabel';
 import Button from '@mui/material/Button';
 import {Grid} from '@material-ui/core';
 import {Autocomplete, TextField, Typography} from "@mui/material";
-import {useDispatch, useSelector} from "react-redux";
-import {fetchCountries} from "../redux/reducers/countryReducer";
 import {addMovie, editMovie} from "../redux/reducers/moviesReducer";
 import useStyles from '../styles/AddEditMovie.style';
+import MovieCatalogServices from "../services/MovieCatalogServices";
+import {useDispatch} from "react-redux";
 
 interface Props {
     setIsEditing: React.Dispatch<React.SetStateAction<boolean>>;
@@ -14,30 +14,18 @@ interface Props {
     movieToEdit: any;
     isAdding: boolean;
 }
-
-interface countryProps {
-    code: string,
-    name: string,
-    uuid: string
-}
-
 export const AddEditMovie: React.FC<Props> = ({setIsEditing, setIsAdding, movieToEdit, isAdding}) => {
     const classes = useStyles();
     const [movieName, setMovieName] = useState<string>('');
-    const [country, setCountry] = useState<countryProps>(isAdding ? {} : movieToEdit?.movieObj?.country ?? {});
-    const allCountries = useSelector((state: any) => state.countries);
+    const [country, setCountry] = useState(isAdding ? {} : movieToEdit?.movieObj?.country ?? {});
+    const [countries, setCountries] = useState([]);
+    const [inputValue, setInputValue] = useState(isAdding ? '' : movieToEdit.movieObj.country.name);
     const dispatch = useDispatch();
-
-    const handleChange = (event: SyntheticEvent<Element, Event>, value: string) => {
-        const country = allCountries.find((country: any) => country.name === value)
-        setCountry(country)
-    };
 
     const handleChangeName = (event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
         const {value} = event.target
         setMovieName(value);
     };
-
 
     const saveMovie = () => {
         const countryUuid: string = country.uuid;
@@ -54,62 +42,35 @@ export const AddEditMovie: React.FC<Props> = ({setIsEditing, setIsAdding, movieT
         setIsEditing(false);
         setIsAdding(false);
     };
-    const [open, setOpen] = useState(false);
-    /*
-    const [newCountries, setNewCountries] = useState([]);
-    const handleOnInputChange = async (event: any) => {
-        setInputValue(event.target.value)
-        dispatch(fetchCountries(inputValue))
-        /!*
-          setNewCountries(response)*!/
-    }*/
 
-    /*  const handleArrowClick = () => {
-        if(allCountries.length === 0){
-            dispatch(fetchCountries())
+
+    const handleInputChange = async (event: any) => {
+        if(event?.target?.value){
+            setInputValue(event?.target?.value);
+            await MovieCatalogServices.getCounties(event.target.value).then((res: any) => {
+                setCountries(res.data)
+            })
         }
 
-        // setNewCountries(allCountries)
-        setOpen(true)
-    }*/
-    /*  const [inputValue, setInputValue] = useState('');
-    const handleOnInputChange = async (event: any) => {
-        setInputValue(event.target.value)
-    }*/
+    };
+    const handleChangeAutoComplete = (event: SyntheticEvent<Element, Event>, value: string) => {
+        const country: any = countries.find((country: any) => country.name === value)
 
-    const CountriesListAutoComplete = () => {
-        const props = {
-            value: country?.name,
-            options: allCountries.map((option: any) => option.name),
-        };
-        return (<Autocomplete
-            {...props}
-            defaultValue={isAdding ? null : movieToEdit.movieObj.country.name}
-            id="countries"
-            size={'small'}
-            open={open}
-            onClose={() => setOpen(false)}
-            onOpen={() => {
-                if (allCountries.length < 1) {
-                    dispatch(fetchCountries())
-                }
-                setOpen(true)
-            }}
-            /*onInputChange={(event, value) => {
-                dispatch(fetchCountries(value))
-            }}*/
-            renderInput={(params) => (
-                <TextField name={'countryUuid'} {...params} variant="outlined"
-                    /*onChange={(event: any) => {
-                    dispatch(fetchCountries(event.target.value))
-                }}*/
-                />
-            )}
-            /* onFocusCapture={() => dispatch(fetchCountries())}*/
-            onChange={(event: SyntheticEvent<Element, Event>, value: string) => handleChange(event, value)}
-            fullWidth
-        />)
-    }
+        setInputValue(country.name);
+        setCountry(country)
+    };
+
+    const load = async () => {
+        await MovieCatalogServices.getCounties().then((res: any) => {
+            setCountries(res.data)
+        })
+    };
+
+    const props = {
+        value: country?.name,
+        options: countries.map((option: any) => option.name),
+        defaultValue: isAdding ? null : movieToEdit.movieObj.country.name
+    };
 
     return (
         <Grid container spacing={1} className={classes.contentContainer}>
@@ -136,12 +97,24 @@ export const AddEditMovie: React.FC<Props> = ({setIsEditing, setIsAdding, movieT
                         <InputLabel>Country</InputLabel>
                     </Grid>
                     <Grid item xs={9}>
-                        <CountriesListAutoComplete/>
+                        <Autocomplete
+                            {...props}
+                            defaultValue={isAdding ? null : movieToEdit.movieObj.country.name}
+                            id="countries"
+                            size={'small'}
+                            inputValue={inputValue}
+                            onInputChange={handleInputChange}
+                            onFocus={load}
+                            renderInput={(params) => (
+                                <TextField name={'countryUuid'} {...params} variant="outlined"
+                                />
+                            )}
+                            onChange={(event: SyntheticEvent<Element, Event>, value: string) => handleChangeAutoComplete(event, value)}
+                            fullWidth
+                        />
                     </Grid>
                 </Grid>
                 <Grid container item xs={10} className={classes.btnSections}>
-                    {/*    <Grid xs={2}/>*/}
-
                     <Grid item xs={3}>
                         <Button sx={{width: 150, height: 30}} variant="contained" type={'submit'}
                                 onClick={saveMovie}>Save</Button>
